64b550cf2b6221c437e8e2cf7be1af0c47189f4f
pandecamillo <160549468+pandecamillo@users.noreply.github.com>
2024-06-06 17:32:53 +0100

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <footer>
       <p>
-        Crée par <a href="https://wa.me/+243826774378">André</a>, <a href="https://wa.me/+243812897044">Ephraïm</a> et <a href="https://wa.me/+243893196755">Camillo</a>
+        Crée par <a href="https://wa.me/+243826114378">André</a>, <a href="https://wa.me/+243816626122">Ephraïm</a> et <a href="https://wa.me/+243893196755">Camillo</a>
       </p>
     </footer>
   );
