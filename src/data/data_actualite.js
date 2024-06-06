f3154504f85c3a875284a82c0fe6a70e66ac2a9a
pandecamillo <160549468+pandecamillo@users.noreply.github.com>
2024-06-06 16:18:23 +0100

    Update data_actualite.js

--- src/data/data_actualite.js
+++ src/data/data_actualite.js
@@ -18,7 +18,7 @@ const data = [
   {
     id: 2,
     image: "../img/actualites/president.jpg",
-    title: "La conversion entre André et le president",
+    title: "Phradel parle au president",
     date: "le 30/01/2024",
     content:
       "Le president de de la republique democratique du congo et le represantant du PAC on discuté à propos du projet tutoré de André, Cette reunion avait pour but de parler de la façon donc la caisse n'est pas prise au serieux. Comment le parti peut ameliorer si les membres ne veulent pas le soutenir ? André avait parlé de la necessité de mettre en place un système de contribution en ligne et sur place afin que les membres puissent contribuer et être recompensé plus tard par rapport à sa contribution. Et pour réprensenter la reussite du meuilleur parti politique de la RDC. La marche a commencé à Kasavubu précisement à l'arret Victoire puis les membres du parti on pris la direction de rond point ngaba jusqu'à arriver à la desitination finale qui est le bureau géneral du parti politique PAC Phradel et Camillo pour rendre l'achesion de membres en ligne ainsi d'automatiser la production de cartes de membres. Ceci sera non seulement pour PAC mais aussi pour tout autres parti politique en RDC. ",
@@ -26,7 +26,7 @@ const data = [
   {
     id: 3,
     image: "../img/actualites/guerre.jpg",
-    title: "La strategie d'André pour gagner",
+    title: "André, Phradel et Camillo ",
     date: "le 25/09/2023",
     content:
       "André a rendu visite au chargé de la sécurité national et on abordé le sujet sur la guerre qui se passe à l'est. << On doit améliorer notre puissance de destruction pour detruire l'énemie >> dit André. André a aussi dit que si on investissait sur l'aspect militaire du pays sur les chercheurs scientifique afin de produire des bombes nucléaires, nous serons une grande puissance de l'Afrique, Le president de de la republique democratique du congo et le represantant du PAC on discuté à propos du projet tutoré de André, Phradel et Camillo pour rendre l'achesion de membres en ligne ainsi d'automatiser la production de cartes de membres. Ceci sera non seulement pour PAC mais aussi pour tout autres parti politique en RDC.",
