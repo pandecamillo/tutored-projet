de5bee7a920dc7ed6aee30688b71d0f1c3784193
pandecamillo <160549468+pandecamillo@users.noreply.github.com>
2024-06-06 16:33:24 +0100

    Update data_actualite.js

--- src/data/data_actualite.js
+++ src/data/data_actualite.js
@@ -5,7 +5,7 @@ const data = [
     title: "La reunion avec André",
     date: "le 3/05/2024",
     content:
-      "Cette reunion avait pour but de parler de la façon donc la caisse n'est pas prise au serieux. Comment le parti peut ameliorer si les membres ne veulent pas le soutenir ? André avait parlé de la necessité de mettre en place un système de contribution en ligne et sur place afin que les membres puissent contribuer et être recompensé plus tard par rapport à sa contribution. Et pour réprensenter la reussite du meuilleur parti politique de la RDC. La marche a commencé à Kasavubu précisement à l'arret Victoire puis les membres du parti on pris la direction de rond point ngaba jusqu'à arriver à la desitination finale qui est le bureau géneral du parti politique PAC",
+      "Cette reunion avait pour but de parler de la façon donc la caisse n'est pas prise au serieux. Comment le parti peut ameliorer si les membres ne veulent pas le soutenir ? André avait parlé de la necessité de mettre en place un système de contribution en ligne et sur place afin que les membres puissent contribuer et être recompensé plus tard par rapport à sa contribution. Et pour réprensenter la reussite du meuilleur parti politique de la RDC nous allons commencer à par créer une application web réservé pour la caisse.",
   },
   {
     id: 1,
@@ -13,7 +13,7 @@ const data = [
     title: "La marche du PAC",
     date: "le 18/02/2024",
     content:
-      "Aujourd'hui le parti politique PAC a fait une marche pour réprensenter la reussite du meuilleur parti politique de la RDC. La marche a commencé à Kasavubu précisement à l'arret Victoire puis les membres du parti on pris la direction de rond point ngaba jusqu'à arriver à la desitination finale qui est le bureau géneral du parti politique PAC, Cette reunion avait pour but de parler de la façon donc la caisse n'est pas prise au serieux.Cette reunion avait pour but de parler de la façon donc la caisse n'est pas prise au serieux. Comment le parti peut ameliorer si les membres ne veulent pas le soutenir ? André avait parlé de la necessité de mettre en place un système de contribution en ligne et sur place afin que les membres puissent contribuer et être recompensé plus tard par rapport à sa contribution. Et pour réprensenter la reussite du meuilleur parti politique de la RDC. La marche a commencé à Kasavubu précisement à l'arret Victoire puis les membres du parti on pris la direction de rond point ngaba jusqu'à arriver à la desitination finale qui est le bureau géneral du parti politique PAC Comment le parti peut ameliorer si les membres.",
+      "Aujourd'hui le parti politique PAC a fait une marche pour réprensenter la reussite du meuilleur parti politique de la RDC. La marche a commencé à Kasavubu précisement à l'arret Victoire puis les membres du parti on pris la direction de rond point ngaba jusqu'à arriver à la desitination finale qui est le bureau géneral du parti politique PAC. Cette marche n'est pas le dernier mais il y en aura encore d'autre dans les années qui suivent. Nous sommes certain que nous allons dominer le monde de la politique en RDC",
   },
   {
     id: 2,
@@ -21,7 +21,7 @@ const data = [
     title: "Phradel parle au president",
     date: "le 30/01/2024",
     content:
-      "Le president de de la republique democratique du congo et le represantant du PAC on discuté à propos du projet tutoré de André, Cette reunion avait pour but de parler de la façon donc la caisse n'est pas prise au serieux. Comment le parti peut ameliorer si les membres ne veulent pas le soutenir ? André avait parlé de la necessité de mettre en place un système de contribution en ligne et sur place afin que les membres puissent contribuer et être recompensé plus tard par rapport à sa contribution. Et pour réprensenter la reussite du meuilleur parti politique de la RDC. La marche a commencé à Kasavubu précisement à l'arret Victoire puis les membres du parti on pris la direction de rond point ngaba jusqu'à arriver à la desitination finale qui est le bureau géneral du parti politique PAC Phradel et Camillo pour rendre l'achesion de membres en ligne ainsi d'automatiser la production de cartes de membres. Ceci sera non seulement pour PAC mais aussi pour tout autres parti politique en RDC. ",
+      "Le president de de la republique democratique du congo et le represantant du PAC on discuté à propos du projet tutoré de André, Phradel et Camillo pour rendre l'achesion de membres en ligne ainsi d'automatiser la production de cartes de membres. Ceci sera non seulement pour PAC mais aussi pour tout autres parti politique en RDC. Sans oublié à propos de la guerre cybernetique qui se passe en Europe, en Amerique et en Russie. Notre parti politique peut aussi en être victime. C'est pourquoi nous nessecitons une grande sécurité informatique de la part de nos chers étudiants de ISS/KIN ",
   },
   {
     id: 3,
@@ -29,7 +29,7 @@ const data = [
     title: "André, Phradel et Camillo ",
     date: "le 25/09/2023",
     content:
-      "André a rendu visite au chargé de la sécurité national et on abordé le sujet sur la guerre qui se passe à l'est. << On doit améliorer notre puissance de destruction pour detruire l'énemie >> dit André. André a aussi dit que si on investissait sur l'aspect militaire du pays sur les chercheurs scientifique afin de produire des bombes nucléaires, nous serons une grande puissance de l'Afrique, Le president de de la republique democratique du congo et le represantant du PAC on discuté à propos du projet tutoré de André, Phradel et Camillo pour rendre l'achesion de membres en ligne ainsi d'automatiser la production de cartes de membres. Ceci sera non seulement pour PAC mais aussi pour tout autres parti politique en RDC.",
+      "Pendant que nous préparons les éxamens à ISS/KIN, André avait profité du blocus pour rendre visite au chargé de la sécurité national et on abordé le sujet sur la guerre qui se passe à l'est. << On doit améliorer notre puissance de destruction pour detruire l'énemie >> dit André. André a aussi dit que si on investissait sur l'aspect militaire du pays sur les chercheurs scientifique afin de produire des bombes nucléaires, nous serons une grande puissance de l'Afrique. Ce message est arrivé jusqu'aux oreilles du president de la république Felix TShisekedi et à décidé d'avoir reunion avec Putine d'ici la fin de ce mois",
   },
 ];
 
